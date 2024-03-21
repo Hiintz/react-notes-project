cafaecccf0ffa4b0e9a862e47a5cc17cbf2295f4
EVRARD Tony <t.evrard@live.fr>
2024-03-21 14:58:54 +0100

    Ajout du loader

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [notes, setNotes] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchNotes = async () => {
+    setIsLoading(true);
+    const response = await fetch('/notes');
+    const data = await response.json();
+
+    setNotes(data);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    
+    fetchNotes();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Notes App</h1>
+        <p>Welcome to the notes app!</p>
+        {isLoading ? (
+          <div>
+            <p>Chargement ...</p>
+          </div>
+        ) : (
+          <ul>
+            {notes.map(note => (
+              <li key={note.id}>{note.title}</li>
+            ))}
+          </ul>
+        )}
       </header>
     </div>
   );
