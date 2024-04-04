9d5532e4e18bef2603c75db171e8e99ba6896a94
EVRARD Tony <t.evrard@live.fr>
2024-04-04 14:57:15 +0200

    ajout du pin, sans sa fonction

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,12 @@
 import React from "react";
 import deleteIcon from "./delete.svg";
+import pinIcon from "./pin.svg";
 import styles from "./Button.module.css";
 
 function Button({ onClick, onDelete, selected, loading, className, children }) {
   return (
     <button onClick={onClick} className={`${styles.button} ${selected ? styles.selected : ""}`}>
+      {selected && <img src={pinIcon} alt="Épingler" className={styles["pin-icon"]} />}
       {selected && (
         <img 
           src={deleteIcon} 
